94de18f628f732bf390cbcbe7d39cdf53b2fd70d
iamcheeseman <xanderswan32@gmail.com>
2023-10-05 15:35:44 -0400

    Stack

---
+++ types/stack.lua
@@ -0,0 +1,28 @@
+local stack = {}
+stack.__index = stack
+
+function stack:push(item)
+  table.insert(self.items, item)
+end
+
+function stack:pop()
+  local item = self.items[#self.items]
+  self.items[#self.items] = nil
+  return item
+end
+
+function stack:flush(f)
+  local item = self:pop()
+  while item do
+    f(item)
+    item = self:pop()
+  end
+end
+
+local function Stack()
+  return setmetatable({
+    items = {},
+  }, stack)
+end
+
+return Stack
